65447c207a8d1dd1a100e72147a23f4ef1445140
Johnny Lee <johnnylee@lawn-128-61-117-220.lawn.gatech.edu>
2018-11-25 10:16:00 -0500

    Adding more string, array, linked list questions

---
+++ src/main/java/RemoveDups.java
@@ -0,0 +1,6 @@
+public class RemoveDups {
+    /*
+    Write code to remove duplicate from an unsorted linked list.
+    How would you solve this problem without a temporary buffer?
+     */
+}
---
+++ src/main/java/ReturnKthttoLast.java
@@ -0,0 +1,5 @@
+public class ReturnKthttoLast {
+    /*
+    Implement an algorithm to find the kth to last element of a singly linked list.
+     */
+}
